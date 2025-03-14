ffbe3f5db5809a25e0150367eb6fd9b5df70993a
BasementKirill <pfeifferlukas@dv-schulen.de>
2025-03-14 12:38:08 +0100

    bug fixes

--- What2Do2Day/obj/Debug/net9.0/What2Do2Day.AssemblyInfo.cs
+++ What2Do2Day/obj/Debug/net9.0/What2Do2Day.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("What2Do2Day")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2d6bffbfce58c934dbf0424d1a61233d3f6284")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45e9c5d8706281eecf16e2f6b3cc124d5754e42")]
 [assembly: System.Reflection.AssemblyProductAttribute("What2Do2Day")]
 [assembly: System.Reflection.AssemblyTitleAttribute("What2Do2Day")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
